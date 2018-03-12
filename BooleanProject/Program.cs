6207d23b4f72d6c70c22e3ecda2d36f53d12bea4
lutfiqaraman <lutfiqaraman@gmail.com>
2018-03-12 21:18:57 +0200

    Done ...

--- BooleanProject/Program.cs
+++ BooleanProject/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
+using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +10,39 @@ namespace BooleanProject
     {
         static void Main(string[] args)
         {
+            bool? stringtoboolean = null;
+
+            Console.WriteLine("Please enter a value");
+
+            string inputvalue = Console.ReadLine();
+            string[] inputoptions = { "", "0", "1" };
+
+            bool notinthearray = inputoptions.Contains(inputvalue);
+
+            if (inputvalue.Length > 1 || (notinthearray == false))
+                Console.WriteLine("Please only 0 or 1 is allowed");
+
+            switch (inputvalue)
+            {
+                case "":
+                    stringtoboolean = null;
+                    break;
+
+                case "0":
+                    stringtoboolean = false;
+                    break;
+
+                case "1":
+                    stringtoboolean = true;
+                    break;
+            }
+            
+            if (stringtoboolean.HasValue)
+                Console.WriteLine(stringtoboolean.Value);
+            else
+                Console.WriteLine("Null");
+
+            Console.ReadKey();
         }
     }
 }
